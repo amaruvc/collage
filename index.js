90d05f9ba4e4c719539a93a6656f568ffbbd25aa
Amaru <amaruvc@gmail.com>
2022-03-19 11:51:06 -0300

    finalización de la tarea

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
 const expressFileUpload = require("express-fileupload");
-const e = require("express");
+const fs = require("fs").promises;
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.static("static"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   expressFileUpload({
     limits: { fileSize: 5242880 },
     abortOnLimit: true,
+
     responseOnLimit: "El archivo ingresado excede el peso máximo (5mb)",
   })
 );
@@ -21,7 +25,29 @@ nunjucks.configure("templates", {
 });
 
 app.get("/", (req, res) => {
-  res.render("index.html", { frutas });
+  res.redirect("/imagen");
+});
+
+app.get("/imagen", (req, res) => {
+  res.render("formulario.html");
+});
+
+app.post("/imagen", async (req, res) => {
+  const data = req.files.target_file.data;
+  const posicion = req.body.posicion;
+
+  await fs.writeFile(`static/imgs/imagen-${posicion}.jpg`, data);
+  res.redirect("/collage");
+});
+
+app.get("/collage", (req, res) => {
+  res.render("collage.html");
+});
+
+app.get("/deleteImg/:img", async (req, res) => {
+  const img = req.params.img;
+  await fs.unlink(`static/imgs/${img}`);
+  res.redirect("/collage");
 });
 
 app.listen(3000, () => {
